8bdf888da134ca38453985823af816ea5009f3c2
Ryan Smith <0ryansmith1994@gmail.com>
2017-12-15 20:56:32 +0000

    ci(circle): Fixes test script.

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
-npm link;
 rm -rf test_project;
 mkdir test_project;
 cd test_project;
-ht2-plop project "test_project" "test_description" "test_org" "circle" || exit 1;
+../node_modules/plop/src/plop.js project "test_project" "test_description" "test_org" "circle" || exit 1;
 
 npm install && \
 npm run build && \
